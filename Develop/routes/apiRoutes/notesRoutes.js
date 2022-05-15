1ff0dceec8d4b34658975a92114637c39bb39e1f
Megan Gedde <hoglu070@d.umn.edu>
2022-05-15 15:23:30 -0500

    added routes

---
+++ Develop/routes/apiRoutes/notesRoutes.js
@@ -0,0 +1,23 @@
+const { notes } = require('./Develop/db/db');
+const { validateNote, createNewNote } = require('../../lib/notes')
+const router = require("express").Router();
+
+router.get('/notes', (req, res) => {
+    res.json(notes);
+  });
+
+router.get('/notes/:title', (req, res) => {
+  const results = findByTitle(req.params.title, notes);
+    res.json(results);
+  }); 
+
+router.post('/notes', (req, res) => {
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+      } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+      }
+});  
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
-const app = express();
+const apiRoutes = require('./Develop/routes/apiRoutes');
+const htmlRoutes = require('./Develop/routes/htmlRoutes');
 const PORT = process.env.PORT || 3001;
 const { notes } = require('./Develop/db/db');
+const app = express();
+  // parse incoming string or array data
+  app.use(express.urlencoded({ extended: true }));
+  // parse incoming JSON data
+  app.use(express.json());
+  app.use(express.static('public'));
+  app.use('/api', apiRoutes);
+  app.use('/', htmlRoutes);
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
-  });
+// function findByTitle(title, notesArray) {
+//   const results = notesArray.filter(notes => notes.title === title)[0];
+//   return results;
+// }
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
